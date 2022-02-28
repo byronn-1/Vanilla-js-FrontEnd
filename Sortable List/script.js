42c8415822db333d66f50579c92b8ef5eb2aebac
Byron Lee <byronn.lee@gmail.com>
2022-02-28 15:05:07 +0000

    added Sortable List project, drag events

---
+++ Sortable List/script.js
@@ -0,0 +1,60 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnanult',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zukerberg',
+  'Michael Bloomberg',
+  'Larry Page'
+];
+
+
+const ListItems = [];
+let draStartIndex;
+
+createList()
+
+function createList() {
+  [...richestPeople]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
+
+      listItem.setAttribute('data-index', index);
+
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+    <p class="person-name">${person}</p>
+    <i class="fas fa-grip-lines"></i>
+    </div>
+    `;
+      ListItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
+  addEventListeners();
+}
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  })
+}
